f7a278357790e52f80f88d351f5b03754c710095
tiago <tiago.moreira@timebrisa.com.br>
2022-09-12 18:50:55 -0300

    feat: :sparkles: Estilização da tabela

--- Chapter02/dtmoney/src/components/Summary/index.tsx
+++ Chapter02/dtmoney/src/components/Summary/index.tsx
@@ -13,7 +13,7 @@ export function Summary(){
                    <img src={incomeImg} alt="Entradas" />
                 </header>
 
-                <strong>R$1000,00</strong>
+                <strong>R$1200,00</strong>
             </div>
 
             <div>
--- Chapter02/dtmoney/src/styles/global.ts
+++ Chapter02/dtmoney/src/styles/global.ts
@@ -7,7 +7,7 @@ export const GlobalStyle = createGlobalStyle`
    --green: #33CC95;
    --blue: #5429CC;
    --blue-light: #6933FF;
-   --text-title: #363F5F
+   --text-title: #363F5F;
    --text-body: #969CB3;
    --shape: #FFFFFF
   }
