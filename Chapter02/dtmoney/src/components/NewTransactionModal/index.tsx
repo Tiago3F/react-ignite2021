8ea6a859c4408794bf1570b8a3dd2780e0ed6a68
tiago <tiago.moreira@timebrisa.com.br>
2022-09-14 10:29:41 -0300

    feat: :sparkles: Estilização dos botoões do formulário

--- Chapter02/dtmoney/src/components/NewTransactionModal/index.tsx
+++ Chapter02/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg';
+import { useState } from 'react';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -8,6 +11,9 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -24,6 +30,33 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <h2>Cadastrar transação</h2>
         <input placeholder='Título' />
         <input type='number' placeholder='Valor' />
+
+
+        <TransactionTypeContainer>
+
+          <RadioBox
+            type="button"  
+            onClick={() => setType('deposit')}
+            isActive={type == 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => setType('witdraw')}
+            isActive={type == 'witdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+
+        </TransactionTypeContainer>
+
+
         <input placeholder='Categoria' />
 
         <button type="submit">Cadastrar</button>
