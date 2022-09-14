4c37a746f70598c4179bc741ed9fd206b81d9724
tiago <tiago.moreira@timebrisa.com.br>
2022-09-14 09:10:00 -0300

    feat: :sparkles: Modal separado

--- Chapter02/dtmoney/src/App.tsx
+++ Chapter02/dtmoney/src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from "./components/Dashboard";
 import Modal from 'react-modal';
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -21,14 +22,13 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
