1963df32e5419b6d26ec3308630e8b6ef76fa8f0
Tiago3F <tiagomoreira3f@gmail.com>
2023-05-15 14:54:12 -0300

    feat: :sparkles: Ambientes de desenvolvimento e produção

--- Chapter01/01-github-explorer/webpack.config.js
+++ Chapter01/01-github-explorer/webpack.config.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
+const isDeveloment = process.env.NODE_ENV !== "production";
+
+
 module.exports = {
-    mode: "development",
+    mode: isDeveloment ? 'development' : 'production',
+    devtool: isDeveloment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, "src", "index.jsx"),
     output: {
         path: path.resolve(__dirname, "dist"),
