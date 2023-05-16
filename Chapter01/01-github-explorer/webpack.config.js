69b53dd82fa2f8d142b4872841e7b9544458030a
Tiago3F <tiagomoreira3f@gmail.com>
2023-05-16 10:57:53 -0300

    feat: :bug: Correção do fast refresh

--- Chapter01/01-github-explorer/webpack.config.js
+++ Chapter01/01-github-explorer/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const isDeveloment = process.env.NODE_ENV !== "production";
 
-
 module.exports = {
-    mode: isDeveloment ? 'development' : 'production',
-    devtool: isDeveloment ? 'eval-source-map' : 'source-map',
+    mode: isDeveloment ? "development" : "production",
+    devtool: isDeveloment ? "eval-source-map" : "source-map",
     entry: path.resolve(__dirname, "src", "index.jsx"),
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -18,17 +18,26 @@ module.exports = {
     },
     devServer: {
         static: path.resolve(__dirname, "public"),
+        hot: true,
     },
     plugins: [
+        isDeveloment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "public", "index.html"),
         }),
-    ],
+    ].filter(Boolean),
     module: {
         rules: [{
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: "babel-loader",
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        plugins: [
+                            isDeveloment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                },
             },
             {
                 test: /\.scss$/,
