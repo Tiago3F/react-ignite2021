591e7d743d68c9328be10f0382f78ceef5c45ffc
Tiago3F <tiagomoreira3f@gmail.com>
2023-05-16 10:36:47 -0300

    feat: :sparkles: Mudança de estado de variável

--- Chapter01/01-github-explorer/src/components/Counter.jsx
+++ Chapter01/01-github-explorer/src/components/Counter.jsx
@@ -1,3 +1,18 @@
-export function name() {
-  
+import { useState } from "react";
+
+export function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div>
+      <h2>{counter}</h2>
+      <button type="button" onClick={increment}>
+        Increment
+      </button>
+    </div>
+  );
 }
