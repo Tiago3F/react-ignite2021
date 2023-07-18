406a2a54d23d42ed3b215da1206949a6134706e4
Tiago3F <tiagomoreira3f@gmail.com>
2023-07-18 12:41:24 -0300

    feat: :sparkles: Static Site Generation

--- Chapter03/ignews/src/pages/index.tsx
+++ Chapter03/ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 
 import styles from "./home.module.scss";
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1NVF9yKVUdhiXIYXEB4c7GlV");
 
   const product = {
@@ -51,5 +51,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
