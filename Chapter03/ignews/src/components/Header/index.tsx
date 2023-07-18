b0d558aeb3692d691bee0a473cba51205b0ea4aa
Tiago3F <tiagomoreira3f@gmail.com>
2023-07-18 10:54:38 -0300

    feat: :sparkles: Componente header

---
+++ Chapter03/ignews/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" width={110} height={40} />
+        <nav>
+          <a className={styles.active} href="/">Home</a>
+          <a href="/posts">Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
