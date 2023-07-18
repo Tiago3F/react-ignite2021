d9372bfb92e6b497f6cfea4e6d2aaef2447bfcad
Tiago3F <tiagomoreira3f@gmail.com>
2023-07-18 11:15:52 -0300

    feat: :sparkles: Componente SignInButton

--- Chapter03/ignews/src/components/Header/index.tsx
+++ Chapter03/ignews/src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -6,9 +7,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" width={110} height={40} />
         <nav>
-          <a className={styles.active} href="/">Home</a>
+          <a className={styles.active} href="/">
+            Home
+          </a>
           <a href="/posts">Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
