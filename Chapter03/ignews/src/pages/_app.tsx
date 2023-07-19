0c8be4b48f34aa2fc59ef6d9d5b1b740b470c92a
Tiago3F <tiagomoreira3f@gmail.com>
2023-07-19 11:31:09 -0300

    feat: :sparkles: Autenticação com Next Auth

--- Chapter03/ignews/src/pages/_app.tsx
+++ Chapter03/ignews/src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 import "../styles/global.scss";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
     </>
   );
 }
